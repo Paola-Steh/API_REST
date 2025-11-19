2705e088e36e2e0b4af856cc25dfbca6d1de719f
Ana Luísa <a.oliveira4@iesb.edu.br>
2025-11-18 21:16:26 -0300

    feat: Configuração Inicial do Projeto  #7

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+// rota de teste
+app.get('/', (req, res) => {
+  res.json({ message: 'API funcionando!' });
+});
+
+module.exports = app;
